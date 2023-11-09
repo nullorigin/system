7e1588351143c65f4e6e2a6702fae926a3259af9
Matthew Fay <125387504+nullorigin@users.noreply.github.com>
2023-11-09 11:44:31 -0500

    Updated env.sh

--- bin/env.sh
+++ bin/env.sh
@@ -1,13 +1,13 @@
 echo "== Checking if the 'aports' repository exists and is up to date =="; \
-if [ -d aports ]; then \
+if [ -d ext/aports ]; then \
     echo "== Found ==" && \
-    cd aports && \
+    cd ext/aports && \
     export GIT_STATUS=$(git status -s -b HEAD); \
     echo ${GIT_STATUS} | \
-         sed -e 's,^.*\origin\/,\"== Your local branch ,g' \
+         sed -e 's,^.*\origin\/,\"== Your local copy ,g' \
         -e 's,\[,is ,g' \
         -e 's,\], commits ==\",g'; \
-    if [ $(echo ${GIT_STATUS} | grep behind) ]; then \
+    if [ $(echo ${GIT_STATUS} | grep "behind") ]; then \
         echo "== Updating ==" && \
         git pull; \
     else \
@@ -15,7 +15,6 @@ if [ -d aports ]; then \
     fi \
 else \
     echo "== Not Found =="; \
-    echo 'Cloning https://git.alpinelinux.org/aports into the 'aports' directory' && \
-    git clone https://git.alpinelinux.org/aports aports --single-branch --no-tags --depth=1 && \
-    cd aports; \
+    echo 'Cloning https://git.alpinelinux.org/aports into 'ext/aports'' && \
+    git clone https://git.alpinelinux.org/aports ext/aports --single-branch --no-tags --depth=1; \
 fi
